6d2cdbda56d177a98cc076213017186cfb629af7
Tarun-AllOps <tarunvasamsetti999@gamil.com>
2025-02-18 21:41:13 -0600

    added 2 more files

---
+++ array.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Here are other names for me"
+read name(3)
+echo "${name[0]}"
+echo "${name[1]}"
+echo "${name[2]}"
---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "username enter chey bro"
+read USERNAME
+echo "Password enter chey bro"
+read -s PASSWORD
+echo "hi ${USERNAME} na peru shell, welcome bro"
