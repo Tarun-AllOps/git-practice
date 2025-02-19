78e93c53c82f20f9af1365534ae6bcdccdfe5005
Tarun-AllOps <tarunvasamsetti999@gamil.com>
2025-02-19 00:09:36 -0600

    added package installation file

--- mysql-installation.sh
+++ mysql-installation.sh
@@ -18,28 +18,26 @@ read package
 
 dnf list installed ${package}
 
-echo "$(echo $?)"
+if [$? -eq 0]
+then
+    echo "${package} is already installed"
+else
+    echo "${package} is not installed..do you want to install it ? if yes please type 'y'";
+    read answer
+    if [${answer} -ne 'y']
+    then
+        echo "you entered ${answer}. aborting installation of the ${package}"
+        exit 1
+    else 
+        echo "Installing the ${package}...."
+    fi
 
-# if [$? -ne 0]
-# then
-#     echo "${package} is not installed..do you want to install it ? if yes please type 'y'";
-#     read answer
-#     if [${answer} -ne 'y']
-#     then
-#         echo "you entered ${answer}. aborting installation of the ${package}"
-#         exit 1
-#     else 
-#         echo "Installing the ${package}...."
-#     fi
-
-#     dnf install ${package} -y
-#     if [$? -eq 0]
-#     then
-#         echo "${package} installation is success"
-#     else
-#         echo "Something went wrong, ${package} installation is failed."
-#         exit 1
-#     fi
-# else
-#     echo "${package} is already installed"
-# fi
+    dnf install ${package} -y
+    if [$? -eq 0]
+    then
+        echo "${package} installation is success"
+    else
+        echo "Something went wrong, ${package} installation is failed."
+        exit 1
+    fi    
+fi
