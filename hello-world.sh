e9483ca1c8e9314f6e62da438d5d639737c2b954
Tarun-AllOps <tarunvasamsetti999@gamil.com>
2025-02-18 21:31:57 -0600

    added variables in the hello-world.sh

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "hi bro ela unnav ?"
+
+echo "username enter chey bro"
+read USERNAME
+echo "Password enter chey bro"
+read -s PASSWORD
+echo "hi ${USERNAME} na peru shell, welcome bro"
