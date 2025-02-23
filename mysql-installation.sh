ff5bd33afaf765f047d4cca092e288835de25228
Tarun-AllOps <tarunvasamsetti999@gamil.com>
2025-02-23 17:41:14 -0600

    functions

--- mysql-installation.sh
+++ mysql-installation.sh
@@ -3,37 +3,43 @@
 echo "Welcome to installation of any package."
 
 UserId=$(id -u);
+CHECK_USER(){
+    if [ $UserId -ne 0 ]
+    then
+        echo "User id is ${UserId}"
+        echo "you are not in super usermode. please do package installation in super usermode"
+        exit 1
+    fi 
+} 
+CHECK_USER
 
-if [ $UserId -ne 0 ]
-then
-    echo "User id is ${UserId}"
-    echo "you are not in super usermode. please do package installation in super usermode"
-    exit 1
-fi  
+PACKAGE_INSTALL(){
+    dnf list installed $1
+
+    if [ $? -eq 0 ]; then
+        echo "$1 is already installed"
+    else
+        echo "$1 is not installed..do you want to install it ? if yes please type 'y'";
+
+        if [ $2 != "y" ]; then
+            echo "you entered $2. aborting installation of the $1"
+            exit 1
+        else 
+            echo "Installing the $1...."
+        fi
+
+        dnf install $1 -y
+
+        if [ $? -eq 0 ]; then
+            echo "$1 installation is success"
+        else
+            echo "Something went wrong, $1 installation is failed."
+            exit 1
+        fi    
+    fi
+}
 
 echo "Enter name of the package you would like to install"
 read package
-dnf list installed $package
 
-if [ $? -eq 0 ]; then
-    echo "$package is already installed"
-else
-    echo "$package is not installed..do you want to install it ? if yes please type 'y'";
-    read answer
-
-    if [ $answer != "y" ]; then
-        echo "you entered $answer. aborting installation of the $package"
-        exit 1
-    else 
-        echo "Installing the $package...."
-    fi
-
-    dnf install $package -y
-
-    if [ $? -eq 0 ]; then
-        echo "$package installation is success"
-    else
-        echo "Something went wrong, $package installation is failed."
-        exit 1
-    fi    
-fi
+PACKAGE_INSTALL $package "y"
