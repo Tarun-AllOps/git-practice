4e4a85fa2b25b374db33f844d39f272c4a87b10c
Tarun-AllOps <tarunvasamsetti999@gamil.com>
2025-02-24 21:47:55 -0600

    test

--- installer.sh
+++ installer.sh
@@ -17,18 +17,18 @@ mkdir -p $logFolder
 
 CHECK_USER(){
     if [ $UserId -ne 0 ];then
-        echo "User id is ${UserId}"
-        echo -e "$r you are not in superuser mode. please do package installation in superuser mode $n"
+        echo "User id is ${UserId}" | tee -a $logFile
+        echo -e "$r you are not in superuser mode. please do package installation in superuser mode $n" | tee -a $logFile
         exit 1
     fi
 }
 
 VALIDATE_INSTALLATER(){
     if [ $1 -ne 0 ]; then
-        echo -e "Something went wrong...$r FAILED $n to install $2"
+        echo -e "Something went wrong...$r FAILED $n to install $2" | tee -a $logFile
         exit 1
     else
-        echo -e "Installation of $2 is $g success $n"
+        echo -e "Installation of $2 is $g success $n" | tee -a $logFile
     fi
 }
 
@@ -47,9 +47,9 @@ for i in $@
 do 
     dnf list installed $i &>>$logFile
     if [ $? -eq 0 ]; then
-        echo -e "$i is $g already installed$n....nothing to do"
+        echo -e "$i is $g already installed$n....nothing to do" | tee -a $logFile
     else
-        echo "$i is not installed...going to install it"
+        echo "$i is not installed...going to install it" | tee -a $logFile
         dnf install $i -y &>>$logFile
         VALIDATE_INSTALLATER $? "$i"
     fi
